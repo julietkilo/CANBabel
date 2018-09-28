70e3fbb0fecbce3ecd3cf796341a6c04ae67bd53
Andre Naujoks <nautsch2@gmail.com>
2018-09-28 15:11:46 +0200

    fixed surefire run (for me)
    reindentation
    
    .java file was reindented by eclipse.
    pom.xml manually consistently indented

--- src/main/java/com/github/canbabel/canio/ui/MainFrame.java
+++ src/main/java/com/github/canbabel/canio/ui/MainFrame.java
@@ -13,7 +13,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
  **/
 package com.github.canbabel.canio.ui;
 // TODO Versionnumber(major.minor.build)
@@ -112,8 +112,6 @@ public class MainFrame extends javax.swing.JFrame {
         for (File dir : dirs) {
             files.addAll(filesForDirectory(dir));
         }
-
-
 
         return files;
     }
